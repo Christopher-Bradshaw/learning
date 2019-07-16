5e513fb0336c0a360ed8588a467ee6c70b57ebec
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2019-07-16 15:45:36 -0700

    good trie!

--- rust/data_structures/src/trie.rs
+++ rust/data_structures/src/trie.rs
@@ -1,51 +1,53 @@
 use std::collections::HashMap;
 
 #[derive(Debug)]
-pub struct Trie {
-    next: HashMap<char, Trie>,
+pub struct Trie<T> {
+    next: HashMap<char, Trie<T>>,
+    val: Option<T>,
 }
 
-impl Trie {
-    pub fn new() -> Trie {
+impl<T> Trie<T>
+where
+    // If the type doesn't implement copy, we run into trouble returning it out of find.
+    // We could maybe return a reference out of find to get around this?
+    // But I like this illustration of generics with restrictions!
+    T: Copy,
+{
+    pub fn new() -> Trie<T> {
         Trie {
             next: HashMap::new(),
+            val: None,
         }
     }
 
-    fn nxt_rst(s: String) -> (char, String) {
-        let (nxt, rst) = (&s[0..1], &s[1..]);
-        let nxt = nxt.chars().next().unwrap();
-
-        (nxt, rst.to_string())
-    }
-
-    pub fn insert(&mut self, val: String) {
-        let (nxt, rst) = Trie::nxt_rst(val);
+    pub fn insert(&mut self, key: String, val: T) {
+        let (nxt, rst) = nxt_rst(key);
 
         if !self.next.contains_key(&nxt) {
             self.next.insert(nxt, Trie::new());
         }
 
-        if rst != "".to_string() {
-            self.next.get_mut(&nxt).unwrap().insert(rst.to_string());
+        if rst == "".to_string() {
+            self.val = Some(val);
+        } else {
+            self.next
+                .get_mut(&nxt)
+                .unwrap()
+                .insert(rst.to_string(), val);
         }
     }
 
-    pub fn contains(&self, val: String) -> bool {
-        let (nxt, rst) = Trie::nxt_rst(val);
+    pub fn find(&self, key: String) -> Option<T> {
+        let (nxt, rst) = nxt_rst(key);
 
-        match self.next.contains_key(&nxt) {
-            true => {
-                if rst == "".to_string() {
-                    return true;
-                } else {
-                    return self.next.get(&nxt).unwrap().contains(rst);
-                }
-            }
-            false => {
-                return false;
-            }
+        if rst == "".to_string() {
+            return self.val;
         }
+
+        return match self.next.get(&nxt) {
+            Some(v) => v.find(rst),
+            None => None,
+        };
     }
 
     pub fn keys(&self) {
@@ -55,20 +57,43 @@ impl Trie {
     }
 }
 
+fn nxt_rst(s: String) -> (char, String) {
+    let (nxt, rst) = (&s[0..1], &s[1..]);
+    let nxt = nxt.chars().next().unwrap();
+
+    (nxt, rst.to_string())
+}
+
 #[cfg(test)]
 mod test {
     use crate::trie::Trie;
 
     #[test]
-    fn basic_functionality() {
+    fn basic_functionality_ints() {
         let mut t = Trie::new();
 
-        t.insert("bear".to_string());
-        t.insert("beat".to_string());
-        t.insert("pear".to_string());
+        t.insert("bear".to_string(), 1);
+        t.insert("beat".to_string(), 1);
+        t.insert("pear".to_string(), 2);
 
-        assert!(t.contains("bear".to_string()));
-        assert!(t.contains("beat".to_string()));
-        assert!(!t.contains("bean".to_string()));
+        assert_eq!(t.find("bear".to_string()).unwrap(), 1);
+        assert_eq!(t.find("beat".to_string()).unwrap(), 1);
+        assert_eq!(t.find("pear".to_string()).unwrap(), 2);
+
+        assert_eq!(t.find("bears".to_string()), None);
+        assert_eq!(t.find("bea".to_string()), None);
+    }
+
+    #[test]
+    fn basic_functionality_floats() {
+        let mut t = Trie::new();
+
+        t.insert("bear".to_string(), 1.);
+        t.insert("pear".to_string(), 2.);
+
+        assert_eq!(t.find("bear".to_string()).unwrap(), 1.);
+        assert_eq!(t.find("pear".to_string()).unwrap(), 2.);
+        assert_eq!(t.find("bears".to_string()), None);
+        assert_eq!(t.find("bea".to_string()), None);
     }
 }
