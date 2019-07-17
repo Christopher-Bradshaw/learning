f866c1979e00d55e3d411db03c5554d0c51751b7
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2019-07-17 16:58:41 -0700

    improve the hash table!

--- rust/data_structures/src/hash_table.rs
+++ rust/data_structures/src/hash_table.rs
@@ -43,23 +43,28 @@ impl HashTable {
         None
     }
 
-    // This needs to take ownership of self, I think, because of resizes and thread safety
-    pub fn insert(mut self, key: String, val: i32) -> HashTable {
+    pub fn insert(&mut self, key: String, val: i32) {
         let slot = self.hash(&key);
 
-        for (i, item) in self.slots[slot].iter().enumerate() {
-            if item.key == key {
-                // We don't need to worry about resizing as we didn't change the length
-                self.slots[slot][i].val = val;
-                return self;
+        match self.in_slot(&key, slot) {
+            Some(i) => self.slots[slot][i].val = val,
+            None => {
+                self.slots[slot].push(HashItem { key, val });
+                self.n_items += 1;
+                if self.n_items == self.n_slots {
+                    self.resize();
+                }
             }
         }
-        self.slots[self.hash(&key)].push(HashItem { key, val });
-        self.n_items += 1;
-        if self.n_items == self.n_slots {
-            return self.resize();
+    }
+
+    fn in_slot(&self, key: &String, slot: usize) -> Option<usize> {
+        for (i, item) in self.slots[slot].iter().enumerate() {
+            if item.key == *key {
+                return Some(i);
+            }
         }
-        return self;
+        return None;
     }
 
     fn hash(&self, key: &String) -> usize {
@@ -68,15 +73,15 @@ impl HashTable {
         (s.finish() % self.n_slots as u64).try_into().unwrap()
     }
 
-    fn resize(self) -> HashTable {
+    fn resize(&mut self) {
         println!("resizing!");
         let mut n = HashTable::new_with_size(self.n_slots * 2);
         for slot in self.slots.iter() {
             for item in slot.iter() {
-                n = n.insert(item.key.clone(), item.val);
+                n.insert(item.key.clone(), item.val);
             }
         }
-        n
+        self.slots = n.slots;
     }
 }
 
@@ -87,10 +92,10 @@ mod test {
     #[test]
     fn basic_functionality_ints() {
         let mut h = HashTable::new();
-        h = h.insert(String::from("b"), 1);
-        h = h.insert(String::from("b"), 2);
-        h = h.insert(String::from("d"), 2);
-        h = h.insert(String::from("e"), 2);
+        h.insert(String::from("b"), 1);
+        h.insert(String::from("b"), 2);
+        h.insert(String::from("d"), 2);
+        h.insert(String::from("e"), 2);
         println!("{:?}", h);
 
         println!("{:?}", h.find(String::from("b")));
