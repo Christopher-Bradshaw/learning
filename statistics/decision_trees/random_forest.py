efae35f7f96b0cb509e90f7769092f8d816e1d17
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2019-07-31 11:59:02 -0700

    finish RF and cleanup notebook

--- statistics/decision_trees/random_forest.py
+++ statistics/decision_trees/random_forest.py
@@ -1,19 +1,25 @@
+import numpy as np
+import scipy.stats
+
 from tree import DecisionTree
 
 
 class RandomForest:
-    def __init__(self, n_trees, tree_config=None):
+    def __init__(self, n_trees, n_cores=1, tree_config=None):
+
         default_tree_config = dict(cut_dim="random_best")
         tree_config = {**(tree_config or {}), **default_tree_config}
 
-        self.trees = [DecisionTree(**tree_config) for i in n_trees]
+        self.trees = [DecisionTree(**tree_config) for i in range(n_trees)]
+        self.n_cores = n_cores
 
     def fit(self, trainX, trainY):
-        for tree in self.trees:
-            tree.fit(trainX, trainY)
+        for t in self.trees:
+            s = np.random.randint(0, len(trainX), len(trainX))
+            t.fit(trainX[s], trainY[s])
 
     def predict(self, testX):
-        preds = []
-        for tree in self.trees:
-            preds.append(tree.predict(testX))
-        return preds
+        preds = [t.predict(testX) for t in self.trees]
+        # This is absurdly slow...
+        # https://github.com/scipy/scipy/issues/1432
+        return scipy.stats.mode(np.array(preds))[0][0]
