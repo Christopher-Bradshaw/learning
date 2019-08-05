0a66b534f5a161ecb12a721fef9ac5101ac7eae1
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2019-08-05 00:56:35 -0700

    allow cross correlations

--- rust/pair_counter/src/naive.rs
+++ rust/pair_counter/src/naive.rs
@@ -1,13 +1,12 @@
-// Currently we only support autocorrelations
-pub fn naive(x: &[f32], y: &[f32], bins: &[f32]) -> std::vec::Vec<i32> {
-    checks(x, y, bins);
+pub fn naive(x1: &[f32], y1: &[f32], x2: &[f32], y2: &[f32], bins: &[f32]) -> std::vec::Vec<i32> {
+    checks(x1, y1, x2, y2, bins);
 
     let mut counts: Vec<i32> = vec![0; bins.len() - 1];
 
     let mut dist: f32;
-    for i in 0..x.len() {
-        for j in i + 1..x.len() {
-            dist = compute_distance(x[i], y[i], x[j], y[j]);
+    for i in 0..x1.len() {
+        for j in i + 1..x2.len() {
+            dist = compute_distance(x1[i], y1[i], x2[j], y2[j]);
             if dist < bins[0] {
                 continue;
             }
@@ -22,9 +21,10 @@ pub fn naive(x: &[f32], y: &[f32], bins: &[f32]) -> std::vec::Vec<i32> {
     return counts;
 }
 
-fn checks(x: &[f32], y: &[f32], bins: &[f32]) {
+fn checks(x1: &[f32], y1: &[f32], x2: &[f32], y2: &[f32], bins: &[f32]) {
     assert!(bins.len() > 1, "Need to provide at least 1 bin");
-    assert_eq!(x.len(), y.len(), "X and Y should be the same length");
+    assert_eq!(x1.len(), y1.len(), "X and Y should be the same length");
+    assert_eq!(x2.len(), y2.len(), "X and Y should be the same length");
 }
 
 fn compute_distance(x1: f32, y1: f32, x2: f32, y2: f32) -> f32 {
@@ -44,10 +44,10 @@ mod test {
         let res;
 
         // Test switching x and y
-        res = naive(&x, &y, &bins);
+        res = naive(&x, &y, &x, &y, &bins);
         assert_eq!(res, vec![2, 2]);
 
-        let res = naive(&y, &x, &bins);
+        let res = naive(&y, &x, &y, &x, &bins);
         assert_eq!(res, vec![2, 2]);
     }
 }
