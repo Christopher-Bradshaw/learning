66a0886e8168118ce9f9d9ca08b06f75db327ee2
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2019-08-09 12:06:10 -0700

    finish the change making program

--- algos/dynamic_programming/change.py
+++ algos/dynamic_programming/change.py
@@ -2,31 +2,44 @@
 The classic dynamic programming problem - how to make change?
 """
 
-memoize = {0: 0}
+# Store the base case - to make 0 requires 0 coins.
+memoize = {0: (0, [])}
 
-# Return the minimumm number of coins required to make this amount of change
+
 def make_change(change, coins):
 
+    # Check if we have already solved this problem (or are at the base case).
+    # This is needed to get O(n) scaling.
     try:
         return memoize[change]
     except KeyError:
         pass
 
+    # Break the problem into all its possible sub-problems and solve those.
     n_coins = []
     for coin in coins:
         if change - coin >= 0:
-            n_coins.append(make_change(change - coin, coins) + 1)
+            sub_problem_n, sub_problem_which = make_change(change - coin, coins)
+            n_coins.append(((sub_problem_n + 1), (sub_problem_which + [coin])))
 
-    n_coins = min(n_coins)
-    memoize[change] = n_coins
-    return n_coins
+    # Choose which of the solutions is best
+    min_idx = None
+    for i, _ in enumerate(n_coins):
+        if min_idx is None:
+            min_idx = i
+        elif n_coins[i][0] < n_coins[min_idx][0]:
+            min_idx = i
+
+    # Memoize the solution to this problem (as it might potentially be a subproblem later) and return it
+    memoize[change] = n_coins[min_idx]
+    return memoize[change]
 
 
 def main():
-    change = 25
-    coins = [1, 5, 8]
+    change = 93
+    coins = [1, 5, 8, 27, 43]
 
-    print(make_change(change, coins))
+    print(f"{change}: {make_change(change, coins)}")
 
 
 if __name__ == "__main__":
