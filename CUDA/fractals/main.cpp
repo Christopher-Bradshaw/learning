f4ba427920d458b5d2ef1a610652e5639aef8e30
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2020-02-25 20:37:43 -0800

    Finished move to constant memory: 477 -> 291 ms

--- CUDA/fractals/main.cpp
+++ CUDA/fractals/main.cpp
@@ -6,7 +6,7 @@
 
 int main(void) {
     cfloat c = {0.5, 0.4};
-    int x_pixels = 1024, y_pixels = 1024;
+    int x_pixels = 4096, y_pixels = 4096;
     int max_value = 200;
     // Allocate the result array
     int *res = (int *)calloc(x_pixels * y_pixels, sizeof(int));
@@ -14,6 +14,6 @@ int main(void) {
     julia::julia_gpu(x_pixels, y_pixels, c, -1.5, 1.5, -1.5, 1.5, max_value,
                      res);
 
-    file_output::write_ppm(res, x_pixels, y_pixels, max_value);
-    file_output::write_jpg(res, x_pixels, y_pixels, max_value);
+    /* file_output::write_ppm(res, x_pixels, y_pixels, max_value); */
+    /* file_output::write_jpg(res, x_pixels, y_pixels, max_value); */
 }
