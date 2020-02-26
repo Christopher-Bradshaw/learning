86eec61401edf6ce85070cc80bff1e4892eaadee
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2020-02-25 18:15:04 -0800

    switch to .cu and output ppm to file

--- CUDA/fractals/file_output.cpp
+++ CUDA/fractals/file_output.cpp
@@ -1,18 +1,22 @@
 #include <Magick++.h>
+#include <fstream>
 #include <iostream>
 
 namespace file_output {
 
 void write_ppm(int **res, int x_pixels, int y_pixels, int max_value) {
-    std::cout << "P2" << std::endl;
-    std::cout << x_pixels << " " << y_pixels << std::endl;
-    std::cout << max_value << std::endl;
+    std::ofstream f;
+    f.open("out.ppm");
+    f << "P2" << std::endl;
+    f << x_pixels << " " << y_pixels << std::endl;
+    f << max_value << std::endl;
     for (int x = 0; x < x_pixels; x++) {
         for (int y = 0; y < y_pixels; y++) {
-            std::cout << res[x][y] << " ";
+            f << res[x][y] << " ";
         }
-        std::cout << std::endl;
+        f << std::endl;
     }
+    f.close();
 }
 
 void write_jpg(int **res, int x_pixels, int y_pixels, int max_value) {
