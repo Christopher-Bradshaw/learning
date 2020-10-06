49532b5fa041c9b11022c7777230e7bdb9e93141
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2020-10-05 22:31:52 -0600

    d3

---
+++ displaying_info/d3/README.md
@@ -0,0 +1,15 @@
+# D3
+
+## Do you want to use D3?
+
+If you just want a static line plot -- D3 is probably overkill. Use another nice JS library with an API like matplotlib's. Create fig, add data.
+
+> The critical thing to keep in mind is that D3 is not a charting library (though it can be used as such). D3 is a framework for building an expressive range of customized and interactive web-based graphics. D3 provides fundamental building blocks for creating just about any visualization you can imagine.
+> D3 in action, 1.7
+
+
+## Examples
+
+* `hello_world.html`: Very basic first example. Adding/selecting/modifying dom elements.
+* `hello_world_svg.html`: Move things around a svg
+* `cities.html`:
---
+++ displaying_info/d3/cities.js
@@ -0,0 +1,55 @@
+console.log("hi");
+
+// Name, pop, happiness
+var cities = [
+    ["Sacramento", 300_000, 0.7],
+    ["San Francisco", 700_000, 0.7],
+    ["New York", 14_000_000, 0.6],
+    ["London", 5_000_000, 0.8],
+    ["Chicago", 6_000_000, 0.3],
+];
+
+const [x_max, y_max] = [500, 500];
+const xscale = d3.scaleLinear()
+    .domain([-0.5, cities.length - 1 + 0.5])
+    .range([0, x_max]);
+
+const yscale = d3.scaleLog()
+    .domain([0.7 * d3.min(cities, el => el[1]), 1.3 * d3.max(cities, el => el[1])])
+    .range([y_max, 0]);
+
+const yaxis = d3.axisRight().scale(yscale).ticks(10).tickSize(10);
+
+const happyscale = d3.scaleLinear().domain([0, 1]).range(["red", "blue"]);
+
+
+// size of the svg
+
+
+window.onload = function () {
+    d3.select("svg")
+        .selectAll("circle")
+        .data(cities)
+        .enter()
+        .append("circle")
+        .attr("r", 20)
+        .attr("cx", (d, i) => xscale(i))
+        .attr("cy", d => (yscale(d[1]))) // 1 - y: as 0 is at the top
+        .style("fill", d => happyscale(d[2]))
+        .on("click", (_, d) => {
+            console.log(d);
+        });
+
+    d3.select("svg")
+        .selectAll("text")
+        .data(cities)
+        .enter()
+        .append("text")
+        .attr("x", (d, i) => xscale(i))
+        .attr("y", y_max)
+        .text(d => d[0]);
+
+    d3.select("svg").append("g").attr("id", "yAxisG").call(yaxis)
+    d3.selectAll("#yAxisG").attr("transform","translate(0,0)")
+
+}
